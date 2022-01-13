a91ada11f0171e10040203506f0c2c938aba615b
Surendar <bug0017@outlook.com>
2022-01-13 14:48:49 +0530

    app-footer ui done

--- src/app/shared/app-shared.module.ts
+++ src/app/shared/app-shared.module.ts
@@ -1,9 +1,10 @@
 import { RouterModule } from '@angular/router';
 import { AppHeaderComponent } from './components/header/app-header.component';
+import { AppFooterComponent } from './components/footer/app-footer.component';
 import { NgModule } from '@angular/core';
 @NgModule({
   imports: [RouterModule],
-  declarations: [AppHeaderComponent],
-  exports: [AppHeaderComponent],
+  declarations: [AppHeaderComponent, AppFooterComponent],
+  exports: [AppHeaderComponent, AppFooterComponent],
 })
 export class AppSharedModule {}
