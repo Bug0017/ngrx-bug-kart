874dccc1af65cb6b223f79049d7c5a7a70348b45
Surendar <bug0017@outlook.com>
2022-01-13 11:59:42 +0530

    ngrx added and init

---
+++ src/app/app-state.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
+import { environment } from 'src/environments/environment';
+@NgModule({
+  imports: [
+    StoreModule.forRoot({
+      router: routerReducer,
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
+  ],
+})
+export class AppStateModule {}
