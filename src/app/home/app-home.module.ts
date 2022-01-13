b2b1f6520e8df30dd815f430714d38c8fbb8fc99
Surendar <bug0017@outlook.com>
2022-01-13 18:10:50 +0530

    type fixed

--- src/app/home/app-home.module.ts
+++ src/app/home/app-home.module.ts
@@ -1,10 +1,12 @@
+import { AppHomePageComponent } from './components/home-page/app-home-page.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AppHomeRoutingModule } from './app-home-routing.module';
-
+import { AppHomeCarouselComponent } from './components/home-carousel/app-home-carousel.component';
 @NgModule({
   imports: [CommonModule, AppHomeRoutingModule],
-  declarations: [],
+  declarations: [AppHomePageComponent, AppHomeCarouselComponent],
   providers: [],
+  exports: [],
 })
 export class AppHomeModule {}
---
+++ src/app/home/components/home-carousel/app-home-carousel.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-carousel',
+  templateUrl: './app-home-carousel.component.html',
+})
+export class AppHomeCarouselComponent {}
